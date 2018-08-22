5ca6a32b7b0184e398789063a0ff59e3da17c780
Tobias Weber <tobias.weber@lrz.de>
2018-08-22 14:16:17 +0200

    [SAI-1148] Added (De/S)erializers for StoreServiceInfo

---
+++ src/main/java/de/gerdiproject/store/data/kafka/serializer/StoreServiceInfoDeserializer.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2018 Nelson Tavares de Sousa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gerdiproject.store.data.kafka.serializer;
+
+import de.gerdiproject.store.data.model.StoreServiceInfo;
+import org.apache.kafka.common.serialization.ByteBufferDeserializer;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class StoreServiceInfoDeserializer implements Deserializer<StoreServiceInfo> {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StoreDataDeserializer.class);
+
+    private final ByteBufferDeserializer bbSerializer = new ByteBufferDeserializer();
+
+    @Override
+    public void configure(Map<String, ?> map, boolean b) {
+        // Nothing to do
+    }
+
+    @Override
+    public StoreServiceInfo deserialize(String topic, byte[] bytes) {
+        try {
+            return StoreServiceInfo.fromByteBuffer(bbSerializer.deserialize(topic, bytes));
+        } catch (IOException e) {
+            LOGGER.error("Error occured while deserializing a StoreData instance.", e);
+        }
+        return null;
+    }
+
+    @Override
+    public void close() {
+        // Nothing to do
+    }
+}
